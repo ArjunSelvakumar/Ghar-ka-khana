50e95ebbcabd38e2a0a406c748b2855e451b6422
Arjun Selvakumar <arjunselva1810@gmail.com>
2022-09-29 12:17:31 +0530

    summa

---
+++ src/main/java/com/food/GharkaKhanaApplication.java
@@ -0,0 +1,14 @@
+package com.food;
+
+import org.springframework.boot.SpringApplication;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GharkaKhanaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GharkaKhanaApplication.class, args);
+    }
+
+}
